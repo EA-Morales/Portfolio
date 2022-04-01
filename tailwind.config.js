46ea555d980a01291d0df3926462d956c066be16
l3gol4s <eduardo.agustin.morales@gmail.com>
2022-04-01 00:03:37 -0300

    daisy ui uninstall

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,5 @@ module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {},
-  },
-  plugins: [require("daisyui")],
-  daisyui: {
-    styled: true,
-    themes: false,
-    base: true,
-    utils: true,
-    logs: true,
-    rtl: false,
-  },
+  },  
 };
