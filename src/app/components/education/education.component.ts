470e96f7ec041bacecb9da5d21f8cc71b1cf1229
l3gol4s <eduardo.agustin.morales@gmail.com>
2022-04-05 03:06:45 -0300

    show education experience

--- src/app/components/education/education.component.ts
+++ src/app/components/education/education.component.ts
@@ -3,15 +3,27 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-education',
   templateUrl: './education.component.html',
-  styleUrls: ['./education.component.scss']
+  styleUrls: ['./education.component.scss'],
 })
 export class EducationComponent implements OnInit {
+  mostrarEducacion: boolean = true;
+  mostrarExperiencia: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  showEducation(): void {
+    if (this.mostrarEducacion === false) {
+      this.mostrarEducacion = true;
+      this.mostrarExperiencia = false;
+    }
   }
 
-  
-
+  showExperience(): void {
+    if (this.mostrarExperiencia === false) {
+      this.mostrarExperiencia = true;
+      this.mostrarEducacion = false;
+    }
+  }
 }
